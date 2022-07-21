5776f56b6caec9caf7e7d06a7c1acd4613a42144
Brandon Pinto <b.pintony@gmail.com>
2022-07-21 14:10:04 -0400

    commit tree 1458c9e2b23c925962dbc5f35bf01aa687bc0026

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+//create a variable employee and make it an object
+//and set it equal to name and streetAddress.
+//
+const employee = {
+    name: "Jack",
+    streetAddress: "112 Washington Street",
+  };
+function updateEmployeeWithKeyAndValue(employee, name) {
+  let newEmployee = {...employee};
+  newEmployee[name] = "11 Broadway";
+  return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+  employee[name] = streetAddress;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name) {
+  let newEmployee = {...employee};
+  delete newEmployee[name]
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+  let newEmployee = employee;
+  delete newEmployee[name];
+  return newEmployee;
+}
